b45d66c6ff6a094dc0607902b254fbe2411ec02a
iman amirul <iahadzim@gmail.com>
2020-12-24 22:16:58 +0800

    7.2 client code updated.

--- LAB6/client/client.py
+++ LAB6/client/client.py
@@ -1,49 +1,22 @@
+import socket
 
+ClientSocket = socket.socket()
+host = 'Your Server IP'
+port = 8888
+	
+print('Waiting for connection')
+try:
+    ClientSocket.connect((host, port))
+except socket.error as e:
+    print(str(e))
 
-#!/usr/bin/python
+Response = ClientSocket.recv(1024)
+print(Response)
+while True:
+    Input = input('Say Something: ')
+    ClientSocket.send(str.encode(Input))
+    Response = ClientSocket.recv(1024)
+    print(Response.decode('utf-8'))
 
-import random
-from multiprocessing import Pool, cpu_count
-from math import sqrt
-from timeit import default_timer as timer
-
-def pi_part(n):
-    print(n)
-
-    count = 0
-
-    for i in range(int(n)):
-
-        x, y = random.random(), random.random()
-
-        r = sqrt(pow(x, 2) + pow(y, 2))
-
-        if r < 1:
-            count += 1
-
-    return count
-
-def main():
-
-    start = timer()
-
-    np = cpu_count()
-    print(f'You have {np} cores')
-
-    n = 100_000_000
-
-    part_count = [n/np for i in range(np)]
-
-    with Pool(processes=np) as pool:
-
-        count = pool.map(pi_part, part_count)
-        pi_est = sum(count) / (n * 1.0) * 4
-
-        end = timer()
-
-        print(f'elapsed time: {end - start}')
-        print(f'π estimate: {pi_est}')
-
-if __name__=='__main__':
-    main()
+ClientSocket.close()
 
